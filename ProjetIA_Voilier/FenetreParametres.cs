710ac4b2ddf8f3c26dab2530ccf1bc57686eee51
RomainBnfn <rbonnefon@esnc.fr>
2020-10-27 10:35:52 +0100

    Début de la partie graphique

---
+++ ProjetIA_Voilier/FenetreParametres.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetIA_Voilier
+{
+    public partial class FenetreParametres : Form
+    {
+        public FenetreParametres()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProjetIA_Voilier/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetIA_Voilier
+{
+    static class Program
+    {
+        public static bool continuer = true;
+
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            while(continuer)
+            { 
+                Application.Run(new FenetreParametres());
+                if (!continuer)
+                    break;
+                Application.Run(new FenetreGraphique());
+            }
+        }
+    }
+}
